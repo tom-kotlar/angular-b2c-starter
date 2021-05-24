762311cd95b434cd10cea01088f6d6e03f9dbaf5
tom-kotlar <tomaskotlar@yahoo.co.uk>
2021-05-24 13:51:29 +0100

    Feat(app.module): MSALInstanceFactory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,42 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
-import { MaterialModule } from '../material/material.module';
+import { MaterialModule } from './material/material.module';
+
+import { IPublicClientApplication, PublicClientApplication, InteractionType } from '@azure/msal-browser';
+import { MsalGuard, MsalBroadcastService, MsalModule, MsalService, MSAL_GUARD_CONFIG, MSAL_INSTANCE, MsalGuardConfiguration, MsalRedirectComponent } from '@azure/msal-angular';
+
+import { msalConfig } from './auth-config';
+import { HomeComponent } from './home/home.component';
+import { GuardedComponent } from './guarded/guarded.component';
+
+
+/**
+ * Here we pass the configuration parameters to create an MSAL instance.
+ * For more info, visit: https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-angular/docs/v2-docs/configuration.md
+ */
+export function MSALInstanceFactory(): IPublicClientApplication {
+  return new PublicClientApplication(msalConfig);
+}
+
+/**
+ * Set your default interaction type for MSALGuard here. If you have any
+ * additional scopes you want the user to consent upon login, add them here as well.
+ */
+export function MSALGuardConfigFactory(): MsalGuardConfiguration {
+  return { 
+    interactionType: InteractionType.Redirect,
+  };
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GuardedComponent
   ],
   imports: [
+    MsalModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
@@ -23,9 +52,21 @@ import { MaterialModule } from '../material/material.module';
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
     }),
-    MaterialModule
+    MaterialModule,
+
   ],
-  providers: [],
+  providers: [  {
+    provide: MSAL_INSTANCE,
+    useFactory: MSALInstanceFactory
+  },
+  {
+    provide: MSAL_GUARD_CONFIG,
+    useFactory: MSALGuardConfigFactory
+  },
+  MsalService,
+  MsalGuard,
+  MsalBroadcastService],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
