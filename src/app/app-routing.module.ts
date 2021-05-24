6d36b7db912e608b6fdad8f00ecc9fec76b1220a
tom-kotlar <tomaskotlar@yahoo.co.uk>
2021-05-24 13:52:20 +0100

    Feat(auth-config): B2c policies

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,45 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MsalGuard } from '@azure/msal-angular';
+import { GuardedComponent } from './guarded/guarded.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'guarded',
+    component: GuardedComponent,
+    canActivate: [
+      MsalGuard
+    ]
+  },
+  {
+    // Needed for hash routing
+    path: 'error',
+    component: HomeComponent
+  },
+  {
+    // Needed for hash routing
+    path: 'state',
+    component: HomeComponent
+  },
+  {
+    // Needed for hash routing
+    path: 'code',
+    component: HomeComponent
+  },
+  {
+    path: '',
+    component: HomeComponent
+  }
+];
+const isIframe = window !== window.parent && !window.opener;
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    useHash: true,
+    // Don't perform initial navigation in iframes
+    initialNavigation: !isIframe ? 'enabled' : 'disabled'
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
